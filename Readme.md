dae2b1a1a7dcfb638b56cf591a41686d4b691a93
Everton Vieira <54176981+gu4xin1m@users.noreply.github.com>
2023-03-29 16:08:24 -0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,19 @@
+
+A script to convert and save Tenable.IO Container Security vulnerabilities in a more friendly and readable xlsx file.
+
+## Install 
+
+`pip install -r requirements.txt`
+
+## Run
+
+Create a file named ".env" with the following variables:
+
+```
+TENABLE_ACCESS_KEY=YOUR_KEY
+TENABLE_SECRET_KEY=YOUR_KEY
+```
+
+Or just the export the env variables.
+
+`python3 run.py`
---
+++ run.py
@@ -0,0 +1,69 @@
+import os
+import pandas as pd
+from tenable.io import TenableIO
+from datetime import datetime
+from dotenv import load_dotenv
+from tqdm import tqdm
+
+
+load_dotenv()
+
+now =  datetime.now().strftime("%Y%m%d-%H%M%S")
+filename = f"Tenable_CS-{now}.xlsx"
+
+TENABLE_ACCESS_KEY = os.getenv('TENABLE_ACCESS_KEY')
+TENABLE_SECRET_KEY = os.getenv('TENABLE_SECRET_KEY')
+
+
+tio = TenableIO(TENABLE_ACCESS_KEY,TENABLE_SECRET_KEY)
+
+def get_report():
+    print("Generating the report...")
+    report = []
+    resp = tio.cs.images.list(return_json=True)
+    for item in tqdm(resp['items']):
+        image = item['name']
+        repo = item['repo_name']
+        tag = item['tag']
+        tioReport = tio.cs.reports.report(repo,image,tag)
+        imageFindings = convert(tioReport,repo)
+        report += imageFindings
+    return report
+
+
+
+def convert(tio_report,repository):
+    image_findings = []
+    for line in tio_report['findings']:
+                finding = line['nvd_finding']
+        
+                #Fixing report structure here !!!
+                #Get Values from parent node
+                finding['image_name'] = tio_report['image_name']
+                finding['repository'] = repository
+                finding['docker_image_id'] = tio_report['docker_image_id']
+                finding['tag'] = tio_report['tag']
+                finding['created_at'] = tio_report['created_at']
+                finding['updated_at'] = tio_report['updated_at']
+                finding['platform'] = tio_report['platform']
+                finding['packages'] = line['packages']
+                finding.pop("packages", None)
+                image_findings.append(finding)
+    return image_findings
+
+
+report = get_report()
+
+
+print("writing to xls file...")
+df = pd.DataFrame.from_dict(report)
+
+#Removing Brackets from 'cpe' and 'references' columns 
+# df['references'] = df.references.apply(lambda x: ', '.join([str(i) for i in x]))
+# df['cpe'] = df.cpe.apply(lambda x: ', '.join([str(i) for i in x]))
+# writer = pd.ExcelWriter(filename,engine='xlsxwriter',engine_kwargs={'options': {'strings_to_urls': False}})
+# df.to_excel(writer,index=False,)
+
+
+df.to_excel(filename, engine='xlsxwriter',index=False,)
+
